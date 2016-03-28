f8441fffcad5d40fbff2b4e2305619e3f6526983
Aundra Miller and nic netzel <netzeln@gmail.com>
2016-03-28 13:13:35 -0700

    add togglability to announcment displays

--- app/components/announcement-tile.js
+++ app/components/announcement-tile.js
@@ -4,7 +4,12 @@ export default Ember.Component.extend({
   isBodyShowing: false,
   actions:{
     showBody() {
+
+      if(this.isBodyShowing){
+      this.set('isBodyShowing', false);
+    } else {
       this.set('isBodyShowing', true);
     }
+  }
   }
 });
