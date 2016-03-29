c24879eaebe5edd4dd5a852a44df6a0ff7020aed
Abigail Rolling and nic netzel <netzeln@gmail.com>
2016-03-29 09:44:19 -0700

    finish homework with styling

--- app/components/announcement-tile.js
+++ app/components/announcement-tile.js
@@ -4,13 +4,12 @@ export default Ember.Component.extend({
   isBodyShowing: false,
   actions:{
     showBody() {
-
       if(this.isBodyShowing){
-      this.set('isBodyShowing', false);
-    } else {
-      this.set('isBodyShowing', true);
-    }
-  },
+        this.set('isBodyShowing', false);
+      } else {
+        this.set('isBodyShowing', true);
+      }
+    },
     delete(announcement){
       if(confirm('Do you Want to Delete this Announcment?')) {
         this.sendAction('deleteAnnouncement', announcement);
